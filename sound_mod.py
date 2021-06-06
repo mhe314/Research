249db5d2c31165367d392cc3e73fd5459c7cc177
Michael He <michaelxh8888@gmail.com>
2021-06-06 16:09:02 -0500

    Working on sound_mod.py

--- sound_mod.py
+++ sound_mod.py
@@ -296,5 +296,5 @@ def guitar_feature_generator(dataset_path, key_name, plot: bool = True):
 
 
 if get_user_data():
-    gen_guitar_feats = pd.DataFrame(guitar_feature_generator(path_dataset, 'B4'))   # list of dictionaries: each with 4 dictionary keys
+    gen_guitar_feats = pd.DataFrame(guitar_feature_generator(path_dataset, 'train\A4'))   # list of dictionaries: each with 4 dictionary keys
     # st.dataframe(gen_guitar_feats)
