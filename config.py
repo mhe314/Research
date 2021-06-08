5f37060091359c56b87ea947472d264cf76a8948
Michael He <michaelxh8888@gmail.com>
2021-06-07 19:13:14 -0500

    Working on sound_mod.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # path_dataset = r'C:\Users\micha\Documents\Pycharm\Research'   # TODO: add in default file path
 # path_default_sound_file = r'C:\Users\micha\Documents\Pycharm\Research\piano\train\A4.wav'  # TODO: add in default file path for .wav file
-path_dataset = r'.'   # TODO: add in default file path
+path_dataset = r''   # TODO: add in default file path
 path_default_sound_file = r'piano\train\A4.wav'  # TODO: add in default file path for .wav file
 
 path_model = 'model.pkl'
