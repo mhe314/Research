9c06e52235629ba839f21ca05b86be01a8b9e594
bradleygoedde <84544495+bradleygoedde@users.noreply.github.com>
2021-06-14 00:42:19 -0700

    Update feature_extractor.py

--- feature_extractor.py
+++ feature_extractor.py
@@ -44,7 +44,7 @@ class FeatureExtractor:
             plt.plot(f, X)
             xlim(0, 3000)  # Define x axis limitation in the figure
             plt.grid()
-            plt.annotate('Dominant Frequencies', xy=(10,20), xytext=(35, 100))
+            plt.annotate('Dominant Frequencies', xy=(10,20), xytext=(500, 200))
             st.pyplot()
 
         # Find fundamental frequencies
