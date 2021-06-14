54c7feef573495d36b47f5d91fbdaa2e1b289f14
bradleygoedde <84544495+bradleygoedde@users.noreply.github.com>
2021-06-14 00:50:29 -0700

    Update feature_extractor.py

--- feature_extractor.py
+++ feature_extractor.py
@@ -44,7 +44,7 @@ class FeatureExtractor:
             plt.plot(f, X)
             xlim(0, 3000)  # Define x axis limitation in the figure
             plt.grid()
-            plt.annotate('Dominant Frequencies', xy=(600,180), xytext=(750, 220), arrowprops=dict(facecolor='black', shrink=20),
+            plt.annotate('Dominant Frequencies', xy=(600,180), xytext=(750, 220), arrowprops=dict(facecolor='black', shrink=15),
             horizontalalignment='left', verticalalignment='top')
             st.pyplot()
 
