f660e950c4a7e644d63ca10854fa5ea4fded3e35
bradleygoedde <84544495+bradleygoedde@users.noreply.github.com>
2021-06-16 16:35:26 -0700

    Update sound_mod.py

--- sound_mod.py
+++ sound_mod.py
@@ -335,5 +335,5 @@ if get_user_data(check1, check2, check3, check4):
         st.title('Plot of Features')
         st.pyplot()
         
-    generated = SoundGenerator(uploaded_file)
+    #generated = SoundGenerator(uploaded_file)
     
