ddb76ee3ed45094b8bb102377c14714001e3531a
bradleygoedde <84544495+bradleygoedde@users.noreply.github.com>
2021-06-16 16:27:02 -0700

    Update sound_mod.py

--- sound_mod.py
+++ sound_mod.py
@@ -295,7 +295,7 @@ def guitar_feature_generator(dataset_path, key_name, plot: bool = True):
                 lns1 = plt.plot(pred_feats[0, :], pred_feats[2, :], '^', label='Prediction (G)')
                 lns2 = plt.plot(true_feats[0, :], true_feats[2, :], 'v', label='Ground Truth (G)')
                 plt.xlabel('Frequency [Hz]', fontsize=16)
-                plt.ylabel('Amplitude', fontsize=16)
+                plt.ylabel('Amplitude, a', fontsize=16)
                 ax2 = ax1.twinx()
                 lns3 = plt.plot(inputs_feats[0, :], inputs_feats[2, :], 'o', c='g', label='Ground Truth (P)')
                 lns = lns1 + lns2 + lns3
@@ -307,7 +307,7 @@ def guitar_feature_generator(dataset_path, key_name, plot: bool = True):
                 lns1 = plt.plot(pred_feats[1, :], pred_feats[3, :], '^', label='Prediction (G)')
                 lns2 = plt.plot(true_feats[1, :], true_feats[3, :], 'v', label='Ground Truth (G)')
                 plt.xlabel('Phase angle [radians]', fontsize=16)
-                plt.ylabel('Dampping coefficient $b_i$', fontsize=16)
+                plt.ylabel('Dampping coefficient, $b_i$', fontsize=16)
                 ax4 = ax3.twinx()
                 lns3 = plt.plot(inputs_feats[1, :], inputs_feats[3, :], 'o', c='g', label='Ground Truth (P)')
                 lns = lns1 + lns2 + lns3
