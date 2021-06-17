a1c7cee62df1e2d2118f237cc5570003f5450844
bradleygoedde <84544495+bradleygoedde@users.noreply.github.com>
2021-06-17 12:45:58 -0700

    Update feature_extractor.py

--- feature_extractor.py
+++ feature_extractor.py
@@ -47,10 +47,10 @@ class FeatureExtractor:
             plt.plot(f, X)
             xlim(0, 3000)  # Define x axis limitation in the figure
             plt.grid()
-#             plt.annotate('Dominant Frequency', xy=(525,265), xytext=(750, 320), arrowprops=dict(facecolor='black'),
-#             horizontalalignment='left', verticalalignment='top')
-            plt.annotate('Dominant Frequency', xy=(xmax,ymax), xytext=(xmax+200, ymax), arrowprops=dict(facecolor='black'),
+            plt.annotate('Dominant Frequency', xy=(525,265), xytext=(750, 320), arrowprops=dict(facecolor='black'),
             horizontalalignment='left', verticalalignment='top')
+#             plt.annotate('Dominant Frequency', xy=(xmax,ymax), xytext=(xmax+200, ymax), arrowprops=dict(facecolor='black'),
+#             horizontalalignment='left', verticalalignment='top')
             plt.annotate('High Frequency', xy=(2250,30), xytext=(2300, 90), arrowprops=dict(facecolor='black'),
             horizontalalignment='left', verticalalignment='top')
             st.pyplot()
