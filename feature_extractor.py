a4d34a30a129ef05d44ef82e84825bbc31d480b1
bradleygoedde <84544495+bradleygoedde@users.noreply.github.com>
2021-06-17 14:19:01 -0700

    Update feature_extractor.py

--- feature_extractor.py
+++ feature_extractor.py
@@ -49,7 +49,7 @@ class FeatureExtractor:
             plt.grid()
 #             plt.annotate('Dominant Frequency', xy=(525,265), xytext=(750, 320), arrowprops=dict(facecolor='black'),
 #             horizontalalignment='left')
-            plt.annotate('Dominant Frequency', xy=(xmax,ymax), xytext=(xmax+300, ymax+5), arrowprops=dict(facecolor='black', shrink=0.1),
+            plt.annotate('Dominant Frequency', xy=(xmax,ymax), xytext=(xmax+300, ymax+15), arrowprops=dict(facecolor='black', shrink=0.1),
             horizontalalignment='left', verticalalignment='top')
             plt.annotate('High Frequency', xy=(2250,30), xytext=(2300, 90), arrowprops=dict(facecolor='black'),
             horizontalalignment='left', verticalalignment='top')
