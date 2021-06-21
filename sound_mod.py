5f4c87edce47e122a48795bb49c00cb115e29098
bradleygoedde <84544495+bradleygoedde@users.noreply.github.com>
2021-06-20 18:08:18 -0700

    Update sound_mod.py

--- sound_mod.py
+++ sound_mod.py
@@ -54,7 +54,7 @@ def get_user_data(check1, check2, check3, check4) -> bool:
         global key
         st.audio(uploaded_file)
         file = str(uploaded_file)
-        key = file(name)
+        key = file[name]
         FeatureExtractor(uploaded_file, check1, check2)
         return True
 
